b3aac83efa6d91f5d928b1bbc0b51b341bf813b6
unknown <smartkelvin66@gmail.com>
2023-08-21 14:09:42 +0100

    updated css

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Form, useLoaderData } from "react-router-dom";
 
 export default function EditContact() {
@@ -22,29 +23,35 @@ export default function EditContact() {
           defaultValue={contact.last}
         />
       </p>
-      <label>
-        <span>Twitter</span>
-        <input
-          type="text"
-          name="twitter"
-          placeholder="@jack"
-          defaultValue={contact.twitter}
-        />
-      </label>
-      <label>
-        <span>Avatar URL</span>
-        <input
-          placeholder="https://example.com/avatar.jpg"
-          aria-label="Avatar URL"
-          type="text"
-          name="avatar"
-          defaultValue={contact.avatar}
-        />
-      </label>
-      <label>
-        <span>Notes</span>
-        <textarea name="notes" defaultValue={contact.notes} rows={6} />
-      </label>
+      <p>
+        <label>
+          <span>Twitter</span>
+          <input
+            type="text"
+            name="twitter"
+            placeholder="@jack"
+            defaultValue={contact.twitter}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span>Avatar URL</span>
+          <input
+            placeholder="https://example.com/avatar.jpg"
+            aria-label="Avatar URL"
+            type="text"
+            name="avatar"
+            defaultValue={contact.avatar}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span>Notes</span>
+          <textarea name="notes" defaultValue={contact.notes} rows={6} />
+        </label>
+      </p>
       <p>
         <button type="submit">Save</button>
         <button type="button">Cancel</button>
